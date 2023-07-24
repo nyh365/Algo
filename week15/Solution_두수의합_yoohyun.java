da996d5c50298f6b675fd8e86c78465b1f4ab8e6
nyh365 <55688466+nyh365@users.noreply.github.com>
2023-07-25 06:50:32 +0900

    Add week15 Solution_두수의합_yoohyun

---
+++ week15/Solution_두수의합_yoohyun.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[] num = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		int K = Integer.parseInt(br.readLine());
+		int start = 0, end = N - 1, result = 0;
+		while(start < end) {
+			int temp = num[start] + num[end];
+			if(temp == K) {
+				start++;
+				end--;
+				result++;
+			} else if(temp < K) {
+				start++;
+			} else {
+				end--;
+			}
+		}
+		
+		System.out.print(result);
+	}
+}
