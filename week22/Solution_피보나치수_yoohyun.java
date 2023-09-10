ec888752b42036c0e85b804f87af2f5ba80ebb07
nyh365 <55688466+nyh365@users.noreply.github.com>
2023-09-10 16:27:10 +0900

    Add week22 Solution_피보나치수_yoohyun

---
+++ week22/Solution_피보나치수_yoohyun.java
@@ -0,0 +1,14 @@
+class Solution {
+    final int mod = 1234567;
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+        
+        dp[0] = 0; dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] %= mod;
+        }
+        
+        return dp[n];
+    }
+}
