a7d7943e9d90d601c5e1d7313318429ff4bfa5b8
nyh365 <55688466+nyh365@users.noreply.github.com>
2023-10-05 22:40:14 +0900

    Add week25 Solution_튜플_yoohyun

---
+++ week25/Solution_튜플_yoohyun.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(String s) {
+        ArrayList<Integer> result = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        s = s.substring(2, s.length() - 2).replace("},{", " ");
+        String[] element = s.split(" ");
+        Arrays.sort(element, (a, b) -> Integer.compare(a.length(), b.length()));
+        
+        String[] tmpFull; int num;
+        for(String e : element){
+            tmpFull = e.split(",");
+            for(String tmp : tmpFull){
+                num = Integer.parseInt(tmp);
+                if(!set.contains(num)){
+                    set.add(num);
+                    result.add(num);
+                }
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+}
