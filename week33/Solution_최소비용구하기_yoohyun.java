cd4a9dda2971062ca6345e1cf828489eeb7d8399
nyh365 <55688466+nyh365@users.noreply.github.com>
2023-11-26 23:07:17 +0900

    Add week33 Solution_최소비용구하기_yoohyun

---
+++ week33/Solution_최소비용구하기_yoohyun.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static class Node implements Comparable<Node>{
+		int vertex, weight;
+		Node next;
+		public Node(int vertex, int weight) {
+			super();
+			this.vertex = vertex;
+			this.weight = weight;
+		}
+		public Node(int vertex, int weight, Node next) {
+			super();
+			this.vertex = vertex;
+			this.weight = weight;
+			this.next = next;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.weight, o.weight);
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		Node[] adjList = new Node[N + 1];
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			
+			adjList[from] = new Node(to, weight, adjList[from]);
+		}
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int des = Integer.parseInt(st.nextToken());
+		
+		int[] minWeight = new int[N + 1];
+		boolean[] check = new boolean[N + 1];
+		Arrays.fill(minWeight, Integer.MAX_VALUE);
+		minWeight[start] = 0;
+		
+		PriorityQueue<Node> pQueue = new PriorityQueue<>();
+		pQueue.offer(new Node(start, minWeight[start]));
+		int cnt = 0;
+		
+		while(!pQueue.isEmpty()) {
+			Node minNode = pQueue.poll();
+			if(check[minNode.vertex]) continue;
+			check[minNode.vertex] = true;
+			if(++cnt == N) break;
+			
+			for(Node tmp = adjList[minNode.vertex]; tmp != null; tmp = tmp.next) {
+				if(!check[tmp.vertex] && minWeight[tmp.vertex] > minWeight[minNode.vertex] + tmp.weight) {
+					minWeight[tmp.vertex] = minWeight[minNode.vertex] + tmp.weight;
+					pQueue.offer(new Node(tmp.vertex, minWeight[tmp.vertex]));
+				}
+			}
+		}
+		
+		System.out.println(minWeight[des]);
+		
+	}
+}
