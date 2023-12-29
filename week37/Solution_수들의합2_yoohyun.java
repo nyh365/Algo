27e7938f693b70185a5c4ffcfd83e9757c83bd0a
nyh365 <55688466+nyh365@users.noreply.github.com>
2023-12-29 17:50:11 +0900

    Add week37 Solution_수들의합2_yoohyun

---
+++ week37/Solution_수들의합2_yoohyun.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int result = 0, total = 0, start = 0;
+		for(int i = 0; i < N; i++) {
+			if(total + arr[i] > M) {
+				while(total + arr[i] > M) {
+					total -= arr[start++];
+				}
+			}
+			total += arr[i];
+			if(total == M) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
+}
